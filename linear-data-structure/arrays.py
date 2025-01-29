1a59359cb07e8cfa219c22647619bef2c09fb142
Dhanush N S <nsdhanush74@gmail.com>
2025-01-29 19:24:55 +0530

    solved porblems based on arrays

--- linear-data-structure/arrays.py
+++ linear-data-structure/arrays.py
@@ -1,6 +1,6 @@
 # Array is collection of elements of same type where stored in a contigous memory location, each element can be accessed usign index and typically the starting of the index is 0
 
-array = [1,2,3,4,5]
+array = [1,2,3,4,5,4,1,3]
 
 #append
 array.append(6)
@@ -32,3 +32,45 @@ def linear_search(array,target):
             return i
     return -1
 print(linear_search(array,3))
+
+
+#function to find the max element in the array
+def max_elem(array):
+    return max(array)
+print(max_elem(array))
+
+#fucntion to reverse an array
+def rev_array(array):
+    return array[::-1]
+print(rev_array(array))
+
+#count occurences
+def occ_array(array,elem):
+    counter = 0
+    for i in array:
+        if i == elem:
+            counter += 1
+    return counter
+elem = 3
+print(occ_array(array,elem))
+
+#combine the sorted arrays
+arr1 = [1,5,3,2,6,4]
+arr2 = [8,10,7,9,12]
+def sort_arr():
+    combined = arr1+arr2
+    combined.sort()
+    return combined
+print(sort_arr()) 
+
+
+
+#Remove duplicates in the array
+a = [1,2,2,4,5,3,3,5,6,7,2,1]
+def array_dup():
+    dup = []
+    for i in a:
+        if i not in dup:
+            dup.append(i)
+    return dup
+print(array_dup())
